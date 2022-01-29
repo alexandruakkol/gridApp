841232fc0593a490f7ef0cccf27cc61a8103ee7e
Alexandru <alexandru.akkol@gmail.com>
2022-01-29 17:39:21 +0200

    did walls

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var index = 0;
 originalDOM = document.body.innerHTML;
+let coordinates = "";
+let wall = ["0"];
 
 function change(e) {
   console.log(e);
@@ -11,7 +13,6 @@ function resetF() {
   x = prompt("Enter X size");
   y = prompt("Enter Y size");
   document.body.innerHTML = originalDOM;
-  create_grid(x, y);
 }
 
 function create_grid(x, y) {
@@ -20,14 +21,25 @@ function create_grid(x, y) {
   for (i = 0; i <= y; i++) {
     body.appendChild(lastDiv);
     for (j = 0; j <= x; j++) {
+        coordinates = i.toString() + "," + j.toString();
+
+        
+
       var newDiv = document.createElement("div");
       newDiv.classList.add("gridInline");
       newDiv.index = index;
-      newDiv.addEventListener("mouseover", function (e) {
-        e.target.style.background = "blue";
-      });
+
+      // Create walls
+      if(wall.includes(coordinates) || i===0 || j===0 || i===19 || j===19){
+        newDiv.style.backgroundColor = "black";
+        
+}else{newDiv.addEventListener("mouseover", function (e) {
+    e.target.style.background = "blue";
+  });}       
+      
       lastDiv.appendChild(newDiv);
       index++;
+      
     }
     lastDiv = document.createElement("div");
     lastDiv.classList.add("gridBlock");
@@ -36,5 +48,5 @@ function create_grid(x, y) {
   reset.addEventListener("click", resetF);
 }
 
-create_grid(15, 15);
+create_grid(19, 19);
 
